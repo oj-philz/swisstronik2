1467c43cb3a6d125888968cc1161ed5d8a9ce446
Sunday Philip Ojogbane <sundayphilip069@gmail.com>
2024-08-30 11:58:04 +0000

    initial commit

---
+++ contracts/DemNiggas.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DemNiggas is ERC721, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("DemNiggas", "DNS")
+        Ownable(msg.sender)
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
---
+++ scripts/mint.js
@@ -0,0 +1,34 @@
+const hre = require("hardhat");
+
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x8537430282aD6c016fC4f1FA252656C44f722837"
+  const [signer] = await hre.ethers.getSigners();
+  const contract = await hre.ethers.getContractAt("DemNiggas", contractAddress, signer);
+  const functionName = "safeMint";
+  const safeMintTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName, [signer.address]),
+    0
+  );
+  await safeMintTx.wait();
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${safeMintTx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
